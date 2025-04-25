a2fdc8c6383fb61c7d815b9eaf7e36e7cf778da3
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-04-25 11:25:08 +0000

    The controllers, middlewares, requests, and models are located in the following directories

--- app/Notifications/NewFollowerNotification.php
+++ app/Notifications/NewFollowerNotification.php
@@ -1,57 +1,24 @@
 <?php
 
-namespace App\Notifications;
+namespace App\Providers;
 
-use App\Models\User;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
+use Illuminate\Support\ServiceProvider;
 
-class NewFollowerNotification extends Notification
+class AppServiceProvider extends ServiceProvider
 {
-    use Queueable;
-
     /**
-     * Create a new notification instance.
+     * Register any application services.
      */
-    public function __construct(protected $user)
+    public function register(): void
     {
         //
     }
 
     /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
+     * Bootstrap any application services.
      */
-    public function via(object $notifiable): array
+    public function boot(): void
     {
-        return ['database'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            'type' => 'follow',
-            'message' => $this->user->name . ' followed to you',
-            'username' => $this->user->username,
-        ];
+        //
     }
 }
